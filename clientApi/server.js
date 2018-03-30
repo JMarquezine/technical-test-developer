dec6cf04bcc85cdfd9afcc16ef107b9221b8089c
Juan Marquezine <juanmarquezine@gmail.com>
2018-03-30 18:20:39 -0300

    Remoção chamada mongo local

--- clientApi/server.js
+++ clientApi/server.js
@@ -1,11 +1,6 @@
 var host = "localhost";
 var porta = 8080;
-
-var mongoose = require('mongoose');
-
 var api = require('./config/express')();
-
-mongoose.connect('mongodb://localhost/clientedb'); 
 
 api.listen(porta, host, function() {
     console.log("Web Api rodando em http://" + host + ":" + porta + "/");
